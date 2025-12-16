5d1425fb7d029f5dc7301a8654fafa4969a9c649
MoYi-Sheep <MoYi-Sheep@LAPTOP>
2025-12-16 16:35:12 +0800

    V1.112 拆分MathML2SVG

--- SimpleReader/src/main.h
+++ SimpleReader/src/main.h
@@ -99,11 +99,7 @@ using namespace Gdiplus;
 #include <blake3.h>
 #include <boost/algorithm/string.hpp>
 
-#include <ft2build.h>
-#include FT_FREETYPE_H
 
-#include FT_OUTLINE_H
-#include FT_GLYPH_H
 #include <Shlobj.h>      // SHGetKnownFolderPath
 #include <KnownFolders.h>
 #include <numeric>
@@ -115,7 +111,7 @@ using namespace Gdiplus;
 
 #pragma comment(lib, "comdlg32.lib")
 #pragma comment(lib, "windowscodecs.lib")
-#pragma comment(lib, "freetype.lib")
+
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "dxgi.lib")
 #pragma comment(lib, "d2d1.lib")
@@ -135,7 +131,7 @@ using namespace Gdiplus;
 #include <unicode/ubrk.h>
 #include <unicode/ustring.h>
 
-
+#include "MML2SVG.h"
 
 using Microsoft::WRL::ComPtr;
 
@@ -1246,116 +1242,6 @@ struct BmpInfo {
 //    }
 //}
 
-
-class MathML2SVG {
-public:
-    static MathML2SVG& instance();
-
-    // 删除拷贝/移动
-    MathML2SVG(const MathML2SVG&) = delete;
-    MathML2SVG& operator=(const MathML2SVG&) = delete;
-    MathML2SVG(MathML2SVG&&) = delete;
-    MathML2SVG& operator=(MathML2SVG&&) = delete;
-
-    /* 业务接口 */
-    std::string convert(const std::string& mathml);
-
-    struct Style {
-        std::string fontSize = "20";
-        std::wstring fontFamily = L"Cambria Math";
-        std::string fill = "#000000";
-        std::string fontStyle;
-        std::string fontWeight;
-    };
-    /* 扩展点 */
-    using AttrMap = std::unordered_map<std::string, std::string>;
-    using RenderFn = std::function<std::string(const tinyxml2::XMLElement*, const Style&)>;
-    using AttrFn = void(*)(const class tinyxml2::XMLAttribute*, class Style&);
-
-    void registerTag(const std::string& tag, RenderFn  fn);
-    void registerAttr(const std::string& attr, AttrFn fn);
-
-private:
-    MathML2SVG();
-    ~MathML2SVG();
-
-    class Impl;
-    std::unique_ptr<Impl> pImpl;
-
-};
-
-
-class GdiTextMeasurer {
-public:
-    static std::wstring makeKey(const std::wstring& name, float size, int style);
-    static GdiTextMeasurer& instance();   // Meyers 单例
-
-    // 返回文本的像素宽高
-    struct Size {
-        float width = 0.f;
-        float height = 0.f;
-        float ascent = 0.f;   // 新增
-    };
-    Size measure(const std::wstring& text,
-        const std::wstring& fontName,
-        float               fontSizePx,
-        Gdiplus::FontStyle  style = Gdiplus::FontStyleRegular);
-
-    std::string outlineToSVG(const std::wstring& text,
-        const std::wstring& fontName,
-        float               fontSizePx,
-        const std::string& fill = "black");
-private:
-    GdiTextMeasurer();
-    ~GdiTextMeasurer();
-    GdiTextMeasurer(const GdiTextMeasurer&) = delete;
-    GdiTextMeasurer& operator=(const GdiTextMeasurer&) = delete;
-
-    struct CachedFont {
-        std::unique_ptr<Gdiplus::FontFamily> family;
-        std::unique_ptr<Gdiplus::Font>       font;
-    };
-
-    std::unordered_map<std::wstring, CachedFont> cache_;
-    std::mutex                                   mtx_;
-};
-
-class FreeTypeTextMeasurer {
-public:
-    static FreeTypeTextMeasurer& instance();   // Meyers 单例
-    struct Size {
-        float width = 0.f;
-        float height = 0.f;
-        float ascent = 0.f;   // baseline → top
-        float descent = 0.f;  // baseline → bottom
-    };
-    Size measure(const std::wstring& text,
-        const std::wstring& fontName,
-        float               fontSizePx,
-        int                 style = 0);   // 0=Regular, 1=Bold, 2=Italic
-
-    std::string outlineToSVG(const std::wstring& text,
-        const std::wstring& fontName,
-        float               fontSizePx,
-        const std::string& fill = "black");
-private:
-    struct CachedFace {
-        FT_Face face = nullptr;
-        float   emSize = 0.f;      // units_per_EM
-    };
-    FreeTypeTextMeasurer();
-    ~FreeTypeTextMeasurer();
-    static FT_Face loadFace(const std::wstring& fontName, int style);
-    CachedFace& getFace(const std::wstring& fontName, int style);
-    FreeTypeTextMeasurer(const FreeTypeTextMeasurer&) = delete;
-    FreeTypeTextMeasurer& operator=(const FreeTypeTextMeasurer&) = delete;
-
-
-    std::unordered_map<std::wstring, CachedFace> cache_;
-    std::mutex                                   mtx_;
-
-    FT_Library ft_ = nullptr;
-};
 
 class BusyGuard {
 public:
